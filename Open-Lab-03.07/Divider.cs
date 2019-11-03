873dbb484261da6893e007d32981699b1bbe8a40
Peterkoricar <korkosk123@gmail.com>
2019-11-03 18:57:41 +0100

    sfgdafsg

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            int a = total % (people * each);
+            if (a != total)
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+            
         }
     }
 }
